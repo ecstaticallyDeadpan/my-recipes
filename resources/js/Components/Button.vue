b04e7ba91d3abd62cb1431d49638156db404901d
Ciaran McAuley <ciaran@ohdigital.co.uk>
2022-05-22 14:03:09 +0100

    Button component

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,22 @@
+<script>
+    import { Link } from '@inertiajs/inertia-vue3';
+    export default {
+        components: {
+            Link
+        }, 
+        props: {
+            text: String,
+            link: String
+        }
+    }
+</script>
+
+<template>
+    <Link
+        type="button"
+        :href="link"
+        class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        {{ text }}
+    </Link>
+</template>
+
