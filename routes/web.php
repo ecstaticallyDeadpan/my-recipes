b493f1e2836ef23b4d79885064ca42610ec0e4f4
Ciaran McAuley <ciaran@ohdigital.co.uk>
2022-05-23 16:52:16 +0100

    Route test for live

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,21 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Dashboard',[
-            'recipes' => Recipe::all()
-        ]);
-    })->name('dashboard');
+
 
     // Recipe index
     Route::get('/recipes', [RecipeIndex::class, 'show'])->name('recipe_index');
